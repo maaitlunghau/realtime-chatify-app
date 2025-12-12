f4e8e2c3d0d6f551f898c2bf0aa1e3f18f7f270d
MTH <chunhau.py@gmail.com>
2025-12-12 14:48:22 +0700

    feat: signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,95 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+
+const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 6 characters"
+            });
+        }
+
+        // check if email valid: regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email format"
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: "Email already exists"
+            });
+        }
+
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                success: true,
+                user: {
+                    _id: newUser._id,
+                    fullName: newUser.fullName,
+                    email: newUser.email,
+                    profilePic: newUser.profilePic
+                }
+            });
+
+            // todos: send a welcome to user
+
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "Invalid user data"
+            });
+        }
+
+    } catch (err) {
+        console.log("Error in signup controller:", err)
+        return res.status(500).json({
+            success: false,
+            message: "Interval server error"
+        });
+    }
+}
+
+const login = (req, res) => {
+    res.send("Login endpoint");
+}
+
+const logout = (req, res) => {
+    res.send("Logout endpoint");
+}
+
+export {
+    signup,
+    login,
+    logout
+}
