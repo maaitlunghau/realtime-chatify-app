5eb446d9af2eb4199c1c531ea8d47b4a8e8a4691
MTH <chunhau.py@gmail.com>
2025-12-12 11:28:43 +0700

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,28 @@
  */
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRouter from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 
 
 dotenv.config();
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 
 const PORT = process.env.PORT || 3000;
