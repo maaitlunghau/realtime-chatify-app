4c55df3c2285fe15cc5d05e42089902c97bf8183
MTH <chunhau.py@gmail.com>
2025-12-16 13:15:21 +0700

    feat: completed the interface and integrate the features into the sidebar

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,6 +1,24 @@
+import { useChatStore } from "../store/useChatStore"
+
 function ActiveTabSwitch() {
+    const { activeTab, setActiveTab } = useChatStore();
+
     return (
-        <div className="text-white">ActiveTabSwitch</div>
+        <div className="tabs tabs-boxed bg-transparent p-2 m-2">
+            <button
+                className={`tab ${activeTab === "chats" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"}`}
+                onClick={() => setActiveTab("chats")}
+            >
+                Chats
+            </button>
+
+            <button
+                className={`tab ${activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"}`}
+                onClick={() => setActiveTab("contacts")}
+            >
+                Contacts
+            </button>
+        </div>
     )
 }
 
